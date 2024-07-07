181ae196ab67007d48045e010aff831a2a150a14
bidhata <pandey.bidhata58@gmail.com>
2024-07-07 15:59:58 +0545

    changes

--- gallery.js
+++ gallery.js
@@ -1,26 +1,28 @@
-
 const slides = document.querySelectorAll(".photo");
 let currentSlide = 0;
 
 function showSlide(index) {
-  slides.forEach((slide, i) => {
-    if (i === index) {
-      slide.style.opacity = 100;
-    } else {
-      slide.style.op
-      acity = 0;
-    }
-  });
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.opacity = 1;
+            slide.style.zIndex = 1;
+        } else {
+            slide.style.opacity = 0;
+            slide.style.zIndex = 0;
+        }
+    });
 }
 
 function nextImage() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  showSlide(currentSlide);
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
 }
 
 function prevImage() {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  showSlide(currentSlide);
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
 }
 
-showSlide(currentSlide);
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentSlide);
+});
